e86950b8368da496ba5df8a54f8e6406638ae01d
VaishooR <vaishooramkrish@gmail.com>
2024-05-10 20:35:11 +0530

    Created movieSlice and custom hook to fetch movie data

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ODg2NTQ4ZGMyZmM5MmIxYWY1ODc2OTgxMmJiYjVhYyIsInN1YiI6IjY2M2UxMmJkYWYyZThmZjQ3NmNkYWJkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f03eUI3tWN08c7oDeCTow4kQcC-bHWMHJNCOeXb_9P0'
+    }
+  };
